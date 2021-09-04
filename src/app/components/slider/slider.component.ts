972dcdf446a8736964f5071e13f37ed508383469
aristidelegba <aristidelegba67@gmail.com>
2021-09-04 20:22:51 +0100

    initial commit

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  constructor() { }
+  @Input() images: any[] = [];
+  ngOnInit(): void {
+  }
+
+}
